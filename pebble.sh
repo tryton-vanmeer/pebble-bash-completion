3d1915634e9d8b47aadcecb83043673ef6728fa3
tryton-vanmeer <tyke505@hotmail.com>
2015-10-25 21:48:12 -0400

    Extended the autocompletion to the install command and the --emulator option

--- pebble.sh
+++ pebble.sh
@@ -1,11 +1,11 @@
-
-_pebble() 
+#!/bin/bash
+_pebble()
 {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    
+
     opts=" \
     new-project \
     convert-project \
@@ -23,10 +23,28 @@ _pebble()
     screenshot \
     coredump"
 
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    #
+    # Complete the arguments to specific commands
+    #
+    case "${prev}" in
+	install)
+		local install_options="--phone --cloudpebble --emulator --serial --qemu"
+		COMPREPLY=( $(compgen -W "${install_options}" -- ${cur}) )
+		return 0
+		;;
+	--emulator)
+		local emulator_options="aplite basalt chalk"
+		COMPREPLY=( $(compgen -W "${emulator_options}" -- ${cur}) )
+		return 0
+		;;
+	*)
+	;;
+    esac
+
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
     return 0
 
 }
 
 complete -F _pebble pebble
-
